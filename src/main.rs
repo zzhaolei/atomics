f8d375c409e9d16ef14e3a542f7b84845f5f1502
zhaolei <im.zhaolei@foxmail.com>
2023-01-06 15:30:31 +0800

    init project

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#[allow(unused)]
+use std::{
+    cell::{Cell, RefCell, UnsafeCell},
+    collections::VecDeque,
+    marker::PhantomData,
+    mem::{ManuallyDrop, MaybeUninit},
+    ops::{Deref, DerefMut},
+    ptr::NonNull,
+    rc::Rc,
+    sync::{
+        atomic::{Ordering::*, *},
+        *,
+    },
+    thread::{self, Thread},
+};
+
+fn main() {
+    println!("Hello, world!");
+}
