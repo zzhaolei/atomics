d4c048ef4932f7b5874ad9e052c10e2fe9d9d3aa
zhaolei <im.zhaolei@foxmail.com>
2023-01-17 10:42:25 +0800

    add channel

---
+++ channel/src/main.rs
@@ -0,0 +1,84 @@
+use std::cell::UnsafeCell;
+use std::marker::PhantomData;
+use std::mem::MaybeUninit;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::{Acquire, Release};
+use std::thread;
+use std::thread::Thread;
+
+struct Sender<'channel, T> {
+    channel: &'channel Channel<T>,
+    receiving_thread: Thread,
+}
+
+struct Receiver<'channel, T> {
+    channel: &'channel Channel<T>,
+    _no_send: PhantomData<*const ()>,
+}
+
+struct Channel<T> {
+    message: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
+}
+
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Channel<T> {
+    const fn new() -> Self {
+        Self {
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
+        }
+    }
+
+    fn split(&mut self) -> (Sender<'_, T>, Receiver<'_, T>) {
+        *self = Self::new();
+        (
+            Sender {
+                channel: self,
+                receiving_thread: thread::current(),
+            },
+            Receiver {
+                channel: self,
+                _no_send: PhantomData,
+            },
+        )
+    }
+}
+
+impl<T> Sender<'_, T> {
+    fn send(self, message: T) {
+        unsafe { (*self.channel.message.get()).write(message) };
+        self.channel.ready.store(true, Release);
+        self.receiving_thread.unpark();
+    }
+}
+
+impl<T> Receiver<'_, T> {
+    fn receive(self) -> T {
+        while !self.channel.ready.swap(false, Acquire) {
+            thread::park();
+        }
+        unsafe { (*self.channel.message.get()).assume_init_read() }
+    }
+}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            unsafe { self.message.get_mut().assume_init_drop() }
+        }
+    }
+}
+
+fn main() {
+    let mut channel = Channel::new();
+    thread::scope(|s| {
+        let (sender, receiver) = channel.split();
+        s.spawn(move || {
+            sender.send("hello");
+        });
+
+        assert_eq!(receiver.receive(), "hello");
+    })
+}
